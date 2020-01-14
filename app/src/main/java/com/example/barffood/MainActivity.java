08a8d9485030a308e542b9a62f7ab0984d22d99c
tomasz <tomasz.szymaniak93@hotmail.com>
2020-01-14 09:09:59 +0100

    Initial commit. Main Menu - basic view for all positions in main menu. *Recipes for each day *Ingredients of your dog diet *Balancing your dog diet (BARF calculator)

---
+++ app/src/main/java/com/example/barffood/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.barffood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onMenuButtonClicked(View view) {
+    }
+
+    public void onIngredientsMenuClicked(View view) {
+    }
+
+    public void onBalanceButtonClicked(View view) {
+        //Intent intent = new Intent(this, ChooseClassIntroActivity.class);
+        //startActivity(intent);
+    }
+}
