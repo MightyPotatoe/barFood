8332778c2e8f214f3c4b8d342042212de857e433
tomasz <tomasz.szymaniak93@hotmail.com>
2020-01-14 09:26:25 +0100

    created activity for diet balancing

---
+++ app/src/main/java/com/example/barffood/dietBalancingActivity.java
@@ -0,0 +1,14 @@
+package com.example.barffood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dietBalancingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_balancing);
+    }
+}
